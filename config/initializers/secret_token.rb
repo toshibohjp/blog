51bacce80ba266f0ac28237854939ab2b7eed40f
toshibohjp <toshiboh.jp@gmail.com>
2013-05-02 11:52:23 +0900

    Create a new blog application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ca1db45959609effc97a14cc4bba0ac92c1631e0c9ebb65539f096582bc1217441d85449cc59e68531b9e5421e93041d472a7b49e8798ba273d7b7e805e1b7a5'
